9e824269ca368ccaf903306c9f0fcd8343543be4
gc1e12 <AARON-MAC@ug-vpn22.ecs.soton.ac.uk>
2013-10-31 14:47:48 +0000

    Update db.php
    create a new variable $db to instantiate a new MySQLi instance and
    connect.

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,10 @@
 <?php
 
 // include file for part 1
+
+	$db =  new mysqli ('comp2203.ecs.soton.ac.uk', 'comp2203-lab5' , 'WL2133r1M7lzp1706Ne', 'comp2203-lab5');
+
+
+	if($mysqli->connect_error){
+		die("could not connect to MYSQL");
+	}

d50cf1c5b0372a60a62f7fc912c05a857c3e91a5
gc1e12 <AARON-MAC@ug-vpn22.ecs.soton.ac.uk>
2013-10-31 14:40:37 +0000

    Update car.class.php
    Update car attribute,
    Update car constructor.

--- classes/car.class.php
+++ classes/car.class.php
@@ -1,3 +1,37 @@
 <?php
 
 // car class for part 1
+
+class car{
+
+	public $id;
+	public $manufacturerId;
+	public $colourId;
+	public $model;
+	public $description;
+	public $price;
+	public $regNumber;
+	public $regDate;
+
+
+	/* 
+	 * This is a contsructor used to create a new car object
+	 *
+	 *@param $id, $manufacturerId, $colourId, $model, $description, $price,$regNumber,$regDate
+	 *@return
+	 *
+	*/
+
+	public function __construct($id, $manufacturerId, $colourId, $model, $description, $price,$regNumber,$regDate){
+
+		this->$id = $id;
+		this->$manufacturerId = $manufacturerId;
+		this->$colourId = $colourId;
+		this->$model = $model;
+		this->$description = $description;
+		this->$price = $price;
+		this->$regNumber = $regNumber;
+		this->$regDate = $regDate;
+	}
+
+}
